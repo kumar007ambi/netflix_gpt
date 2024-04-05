03a2b9e65965cc79c61571edb3ce2cb1798c20fd
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-04-05 22:11:23 +0530

    added some css

--- src/components/SecondaryMainContainer.js
+++ src/components/SecondaryMainContainer.js
@@ -6,13 +6,20 @@ const SecondaryMainContainer = () => {
   const movies = useSelector((store) => store.movies);
   //console.log(movies);
   return (
-    <div>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-      <MovieList title={"Popular"} movies={movies.popularMovies} />
-      <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
-      <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
-    </div>
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="-mt-40 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
   );
 };
 
