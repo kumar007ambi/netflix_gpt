87d03245e946ad4f18c729091f8ed971da978dbe
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2024-04-15 20:23:55 +0530

    minor chnages

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -70,7 +70,7 @@ const GptSearchBar = () => {
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
         <button
-          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          className="col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg"
           onClick={handleGptSearchClick}
         >
           {lang[langKey].search}
